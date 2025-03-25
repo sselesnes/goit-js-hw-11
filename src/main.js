d190bc707dfd2f3f3b8f4a7c68fb88ca971bb14d
Yuri Boblak <sseslesnes@gmail.com>
2025-03-25 17:47:06 +0200

    form handler

--- src/main.js
+++ src/main.js
@@ -1,2 +1,18 @@
-import func1 from './js/pixabay-api';
-import func2 from './js/render-functions';
+// import func1 from './js/pixabay-api';
+// import func2 from './js/render-functions';
+
+const formSearch = document.querySelector('.form');
+const searchText = formSearch.elements['search-text'];
+
+formSearch.addEventListener('submit', event => {
+  event.preventDefault();
+  console.log(searchText.value);
+});
+
+searchText.addEventListener('blur', event => {
+  event.target.focus();
+});
+
+window.addEventListener('load', () => {
+  searchText.focus();
+});
