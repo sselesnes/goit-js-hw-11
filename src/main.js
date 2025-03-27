b8256b917dac943641840bd8732607ff31aaa018
Yuri Boblak <160606809+sselesnes@users.noreply.github.com>
2025-03-27 20:36:30 +0200

    add url ?q=searchquery

--- src/main.js
+++ src/main.js
@@ -3,32 +3,36 @@ import 'izitoast/dist/css/iziToast.min.css';
 import fetchImages from './js/pixabay-api';
 import renderGallery from './js/render-functions';
 
-const formHandler = () => {
-  const searchForm = document.querySelector('.form');
-  const searchQuery = searchForm.elements['search-text'];
-  searchQuery.autocomplete = 'off';
+const searchForm = document.querySelector('.form');
+const searchQuery = searchForm.elements['search-text'];
+searchQuery.autocomplete = 'off';
 
+const requestProcessing = () => {
+  document.querySelector('.gallery')?.remove();
+  const cssLoader =
+    document.querySelector('.loader') ?? document.createElement('span');
+  cssLoader.classList.add('loader');
+  searchForm.insertAdjacentElement('afterend', cssLoader);
+
+  fetchImages(searchQuery.value).then(fetchResultJSON => {
+    cssLoader.remove();
+    if (fetchResultJSON.totalHits) {
+      renderGallery(fetchResultJSON, searchForm);
+    } else {
+      iziToast.error({
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+        position: 'topRight',
+        timeout: 2000,
+      });
+    }
+  });
+};
+
+const formHandler = () => {
   searchForm.addEventListener('submit', event => {
     event.preventDefault();
-    document.querySelector('.gallery')?.remove();
-    const cssLoader =
-      document.querySelector('.loader') ?? document.createElement('span');
-    cssLoader.classList.add('loader');
-    searchForm.insertAdjacentElement('afterend', cssLoader);
-
-    fetchImages(searchQuery.value).then(fetchResultJSON => {
-      cssLoader.remove();
-      if (fetchResultJSON.totalHits) {
-        renderGallery(fetchResultJSON, searchForm);
-      } else {
-        iziToast.error({
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-          position: 'topRight',
-          timeout: 2000,
-        });
-      }
-    });
+    requestProcessing();
   });
 
   const searchFocus = () => searchQuery.focus();
@@ -37,4 +41,9 @@ const formHandler = () => {
   document.body.addEventListener('keydown', () => searchFocus());
 };
 
+const urlParams = new URLSearchParams(window.location.search).get('q');
+if (urlParams) {
+  searchQuery.value = urlParams;
+  requestProcessing();
+}
 formHandler();
