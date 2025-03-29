3b56f42a3c3185746ff50b30c172b4632b5f6594
Yuri Boblak <160606809+sselesnes@users.noreply.github.com>
2025-03-29 08:26:08 +0200

    fetchimages .catch error cosmetics

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 export default async function fetchImages(searchQuery) {
   const myApiKey = '49525829-4ad651e5c3f704318c87db2e9';
-  const url = 'https://1pixabay.com/api/';
+  const url = 'https://pixabay.com/api/';
 
   try {
     const response = await axios.get(url, {
